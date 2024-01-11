4dccd22d451bd8f42c859a410363fd1be104df6e
Fadel Ellah ERRAMI <94233481+FADELELLAHerrami@users.noreply.github.com>
2024-01-11 02:09:35 +0100

    first commit
    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push , onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+
+
+const appSettings = {
+    databaseURL: "https://bread-6d3fe-default-rtdb.europe-west1.firebasedatabase.app/"
+
+}
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const movieInDb = ref(database,"movie");
+const bookInDb = ref(database,"books");
+
+
+onValue(movieInDb, function (snapshot) {
+    document.querySelector('#shopping-list').innerHTML ="";
+    console.log("snapshot");
+    // console.log(snapshot.val()); // Use snapshot.val() to get the data
+    let movies = Object.values(snapshot.val());
+    movies.forEach((element)=>{
+        addItemToUl(element);
+    })
+    console.log("snapshot");
+});
+
+
+let button = document.querySelector("#add-button");
+let input = document.querySelector("#input-field");
+button.addEventListener("click",()=>{
+    let inputContent = input.value;
+    push(movieInDb, inputContent);
+    
+});
+
+
+// create a function to add an li to ul with id shopping-list
+function addItemToUl(content){
+    let element = document.createElement("li");
+    element.textContent=content;
+    let ul = document.querySelector('#shopping-list');
+    ul.appendChild(element);
+}

f2c8b4503adea8e51a3ebff7c837c8c3acff4a0d
Fadel Ellah ERRAMI <94233481+FADELELLAHerrami@users.noreply.github.com>
2024-01-12 07:59:00 +0100

    remove an item from firebase

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push , onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push , onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 
 
@@ -11,19 +11,15 @@ const appSettings = {
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
 const movieInDb = ref(database,"movie");
-const bookInDb = ref(database,"books");
 
 
 onValue(movieInDb, function (snapshot) {
     document.querySelector('#shopping-list').innerHTML ="";
-    console.log("snapshot");
     // console.log(snapshot.val()); // Use snapshot.val() to get the data
     let movies = Object.entries(snapshot.val());
-    movies.forEach((element)=>{
-        element.forEach((e)=>{
-            addItemToUl(element[1],element[0]);
-        })
-        
+    const moviesEntries =  movies.map((element)=> element)
+    moviesEntries.forEach((e)=>{
+        addItemToUl(e[1],e[0]);
     })
     
 });
@@ -31,6 +27,7 @@ onValue(movieInDb, function (snapshot) {
 
 let button = document.querySelector("#add-button");
 let input = document.querySelector("#input-field");
+
 button.addEventListener("click",()=>{
     let inputContent = input.value;
     push(movieInDb, inputContent);
@@ -43,7 +40,10 @@ function addItemToUl(content,key){
     element.textContent=content;
     let ul = document.querySelector('#shopping-list');
     ul.appendChild(element);
+    // remove item
     element.addEventListener("click",(e)=>{
         console.log(key)
+        let exactLocationFromDb = ref(database, `movie/${key}`);
+        remove(exactLocationFromDb);
     })
 }
